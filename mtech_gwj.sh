667565bdf036e3844d3c18f279e4f1129573b897
GideonJonesMTECH <69857043+GideonJonesMTECH@users.noreply.github.com>
2020-08-20 08:01:44 -0600

    Init Commit
    Initial Commit. The File *does* work, but I don't think it does exactly what I want it to.
    Username: Gideon
    Password: NotWebDev

---
+++ mtech_gwj.sh
@@ -0,0 +1,55 @@
+textfile1=file1.txt
+textfile2=file2.txt
+
+if [[ -z ${1} ]]; then
+  echo "you need to pass a param on the cli"
+  exit
+else echo "value of positional param:" ${1}
+fi
+
+
+echo "What's your username?"
+read username
+echo "What's your password?"
+read password
+
+function checkAuthentification {
+  if [[ ${username} != "Gideon" ]] || [[ ${password} != "NotWebDev" ]]; then
+    echo "You should not be here!"
+    exit 1
+  else
+    echo "Welcome Gideon..."
+  fi
+
+  echo "Authentication accepted, Thanks" ${username} ". Now, let's get to that script."
+}
+
+checkAuthentification
+
+function Code {
+  echo "The Script is Starting..."
+
+  cd ~/Desktop
+  mkdir newDir
+  cd newDir
+  echo "You should read Steelheart By Brandon Sanderson" > ${textfile1}
+#Move file (defined as variable 1 in script) to the directory you started your script (i.e. ~/Desktop).
+  mv file1.txt ..
+  pwd
+  cd ..
+  mkdir TXT
+  echo ${1} > ${textfile2}
+  mv file1.txt TXT
+  mv file2.txt TXT
+  rm -rf newDir
+  cd TXT
+  ls
+  pwd
+  cd ..
+  cat TXT
+  echo “Script finished. If variable 1 and variable 2 are in TXT folder you passed”
+  cd ~/Desktop/MTECH
+  sh mtech_gwj.sh 1
+}
+
+  Code
